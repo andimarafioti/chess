d5f4cffcd4b656860b0c69ff3192b49bab22cd47
filsino <andimarafioti@gmail.com>
2017-08-01 19:44:12 -0300

    passed the seventh test

--- game/pieces/pawn.py
+++ game/pieces/pawn.py
@@ -3,4 +3,7 @@ from game.pieces.piece import Piece
 
 class Pawn(Piece):
 	def canMoveFromTo(self, anInitialRow, anInitialColumn, aNewRow, aNewColumn):
-		return True
+		return self._movementIsOneOrTwoStepsInAnyDirection(anInitialRow, anInitialColumn, aNewRow, aNewColumn)
+
+	def _movementIsOneOrTwoStepsInAnyDirection(self, anInitialRow, anInitialColumn, aNewRow, aNewColumn):
+		return anInitialColumn == aNewColumn and 0 < abs(anInitialRow - aNewRow) < 3
