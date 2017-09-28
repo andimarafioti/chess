69cb82054243942279a04acd346fcc6aa098bd37
amarafioti <andimarafioti@gmail.com>
2017-09-28 17:19:17 +0200

    Refactor to the pawn for it to be closer to the behaviour of the rest of the pieces

--- game/pieces/pawn.py
+++ game/pieces/pawn.py
@@ -2,29 +2,23 @@ from game.pieces.piece import Piece
 
 
 class Pawn(Piece):
-	COUNT_OF_SPACES_I_CAN_MOVE_FORWARD = 2
-
 	def canApplyAMovement(self, aMovement):
 		return aMovement.initialColumn() == aMovement.newColumn() and \
 			self._movementIsOneOrTwoStepsInTheRightDirection(aMovement)
 
 	def _movementIsOneOrTwoStepsInTheRightDirection(self, aMovement):
-		if self._isWhite:
-			condition = 0 < aMovement.newRow() - aMovement.initialRow() <= self.COUNT_OF_SPACES_I_CAN_MOVE_FORWARD
+		if self.HAS_MOVED:
+			return self._movementIsXStepsInTheRightDirection(aMovement, 1)
 		else:
-			condition = 0 < aMovement.initialRow() - aMovement.newRow() <= self.COUNT_OF_SPACES_I_CAN_MOVE_FORWARD
-		return condition
+			return self._movementIsXStepsInTheRightDirection(aMovement, 2)
 
-	def _movementIsOneStepInTheRightDirection(self, aMovement):
+	def _movementIsXStepsInTheRightDirection(self, aMovement, x):
 		if self._isWhite:
-			condition = 0 < aMovement.newRow() - aMovement.initialRow() <= 1
+			condition = 0 < aMovement.newRow() - aMovement.initialRow() <= x
 		else:
-			condition = 0 < aMovement.initialRow() - aMovement.newRow() <= 1
+			condition = 0 < aMovement.initialRow() - aMovement.newRow() <= x
 		return condition
-
-	def appliedPlay(self, aPlay):
-		self.COUNT_OF_SPACES_I_CAN_MOVE_FORWARD = 1
 
 	def canCaptureAnotherPieceWithAMovement(self, aMovement):
 		return 0 < abs(aMovement.initialColumn() - aMovement.newColumn()) < 2 and \
-			self._movementIsOneStepInTheRightDirection(aMovement)
+			self._movementIsXStepsInTheRightDirection(aMovement, 1)
